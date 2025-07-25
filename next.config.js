7738499c6496385d8b7c73d0460c7442b66bdb59
yhsmoa <yhsmoa@naver.com>
2025-07-25 11:07:57 +0800

    Fix configuration

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
-  },
-  // 외부 접속 허용 설정
-  hostname: '0.0.0.0',
-  port: 3000
+  }
 }
 
 module.exports = nextConfig 
